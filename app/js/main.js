a81529f69292051899b0b8b4930199eec38d89a1
Fritosauripo <antonov.vladislav.derrik@gmail.com>
2021-03-15 22:27:21 +0200

    mixit up plugin added

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,15 @@ $(function(){
     arrows: false,
     dots: true,
   });
+
+  var mixer = mixitup('.product-filter__content', {
+
+    "animation": {
+      "duration": 250,
+      "nudge": true,
+      "reverseOut": false,
+      "effects": "fade translateZ(-100px)"
+    }
+
+  });
 })
