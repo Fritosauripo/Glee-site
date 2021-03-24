52d9a01b7ad67501c17e5819791106e80c3546ff
Fritosauripo <antonov.vladislav.derrik@gmail.com>
2021-03-24 22:30:28 +0200

    final commit

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,11 @@ $(function(){
     dots: true,
   });
 
+  $('.product-sponsors__inner').slick({
+    arrows: false,
+    dots: false
+  });
+
   var mixer = mixitup('.filter__content', {
 
     "animation": {
@@ -12,6 +17,26 @@ $(function(){
       "reverseOut": false,
       "effects": "fade translateZ(-100px)"
     }
+  });
 
+  $('.product-video').magnificPopup({
+    items: [
+      {
+        src: 'https://www.youtube.com/watch?v=wk22xOm1TNo',
+        title: 'Peter & Paul fortress in SPB'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'iframe' // this is a default type
+});
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 400) {
+      $('.header').css('box-shadow', '0px 4px 8px 0px rgba(0, 0, 0, 0.22)');
+    } else {
+      $('.header').css('box-shadow', '0px 4px 8px 0px rgba(0, 0, 0, 0)');
+    }
   });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ function scripts() {
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/mixitup/dist/mixitup.js',
+      'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
